1f5ca627657f6a59198dc8c485038f8e82e68162
Schlatt Jannik <jschla@d-velop.de>
2018-08-29 15:49:07 +0200

    (2) I added the JUnit dependency and created a JUnit test case for the Main class' method returnZero(). The test should fail.

---
+++ src/test/java/jannikschlatt/bamboo_test/test/MainTest.java
@@ -0,0 +1,20 @@
+package jannikschlatt.bamboo_test.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+    private Main myTarget;
+    
+    @Before
+    public void before() {
+	myTarget = new Main();
+    }
+    
+    @Test
+    public void testReturnZero() {
+	fail("Not yet implemented");
+    }
+}

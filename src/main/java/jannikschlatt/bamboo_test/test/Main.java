3bccfbba6d4787d968c1c8bc3b5ced28c0a6d781
Schlatt Jannik <jschla@d-velop.de>
2018-08-29 15:46:19 +0200

    I added the JUnit dependency and created a JUnit test case for the Main class' method returnZero(). The test should fail.

--- src/main/java/jannikschlatt/bamboo_test/test/Main.java
+++ src/main/java/jannikschlatt/bamboo_test/test/Main.java
@@ -1,7 +1,11 @@
 package jannikschlatt.bamboo_test.test;
 
 public class Main {
+    public int returnZero() {
+	return 0;
+    }
+    
     public static void main(String[] args) {
-	bla
+//	bla
     }
 }
